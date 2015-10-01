f52e152363c810ce646dd902c578099632ac170b
code.nchudnovets <code.nchudnovets@gmail.com>
2015-10-01 03:26:46 -0400

    js lesson1 hw added

---
+++ GoIT/js/home/lesson1/script2.js
@@ -0,0 +1,8 @@
+var name = prompt('What is your name?');
+
+if (name !== '') {
+    alert('Your name is ' + name);
+    }
+else {
+    alert("That's wrong! You didn't eneter your name.");
+};
---
+++ GoIT/js/home/lesson1/script3.js
@@ -0,0 +1,5 @@
+var name = prompt('What is your name?');
+console.log(name);
+
+var isTrue = confirm('Is it really your name?');
+console.log(isTrue);
